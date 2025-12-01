ca9d8d5d038f3ca5ab219c5abc553480faa86467
Akbar Karimov <akbarkarimov250@gmail.com>
2025-12-01 13:50:33 +0000

    Fix storage.h

--- include/storage.h
+++ include/storage.h
@@ -1,9 +1,11 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 
+#include <stdbool.h>
+#include <stdint.h>
 #include "app_config.h"
 
-#define FLASH_STORAGE_ADDR 0x000F0000
+#define FLASH_STORAGE_ADDR 0x00070000
 
 bool storage_read_hsv(hsv_color_t *hsv);
 
