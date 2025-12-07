cd11b90c611b51620a8ed36b2a024419557219e0
Akbar Karimov <akbarkarimov250@gmail.com>
2025-12-07 12:56:01 +0000

    Update hsv.c for Workshop 7

--- src/hsv.c
+++ src/hsv.c
@@ -43,3 +43,41 @@ void hsv_to_rgb_simple(uint16_t h, uint8_t s, uint8_t v, uint16_t *r, uint16_t *
             break;
     }
 }
+
+void rgb_to_hsv_simple(uint16_t r, uint16_t g, uint16_t b, uint16_t *h, uint8_t *s, uint8_t *v)
+{
+    if (r > PWM_TOP_VALUE) r = PWM_TOP_VALUE;
+    if (g > PWM_TOP_VALUE) g = PWM_TOP_VALUE;
+    if (b > PWM_TOP_VALUE) b = PWM_TOP_VALUE;
+
+    uint32_t max_val = MAX(r, MAX(g, b));
+    uint32_t min_val = MIN(r, MIN(g, b));
+    uint32_t delta = max_val - min_val;
+
+    *v = max_val / 10; 
+
+    if (max_val == 0) {
+        *s = 0;
+    } else {
+        *s = (delta * 100) / max_val;
+    }
+
+    if (delta == 0) {
+        *h = 0;
+    } else {
+        int32_t hue_temp;
+        if (max_val == r) {
+            hue_temp = ((int32_t)(g - b) * 60) / (int32_t)delta;
+            if (g < b) hue_temp += 360;
+        } else if (max_val == g) {
+            hue_temp = ((int32_t)(b - r) * 60) / (int32_t)delta + 120;
+        } else {
+            hue_temp = ((int32_t)(r - g) * 60) / (int32_t)delta + 240;
+        }
+        
+        if (hue_temp < 0) hue_temp += 360;
+        if (hue_temp >= 360) hue_temp -= 360;
+        
+        *h = (uint16_t)hue_temp;
+    }
+}
