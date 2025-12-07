03bbd0c5f192f19a7730f81c5384d2bb75da6b49
Akbar Karimov <akbarkarimov250@gmail.com>
2025-12-07 12:55:40 +0000

    Update hsv.h for Workshop 7

--- include/hsv.h
+++ include/hsv.h
@@ -5,4 +5,6 @@
 
 void hsv_to_rgb_simple(uint16_t h, uint8_t s, uint8_t v, uint16_t *r, uint16_t *g, uint16_t *b);
 
+void rgb_to_hsv_simple(uint16_t r, uint16_t g, uint16_t b, uint16_t *h, uint8_t *s, uint8_t *v);
+
 #endif
