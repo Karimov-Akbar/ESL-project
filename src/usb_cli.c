8899f67a3eb0b935105938c49ecf301e016da0c1
Akbar Karimov <akbarkarimov250@gmail.com>
2025-12-08 07:44:06 +0000

    Fixes in usb_cli.c

--- src/usb_cli.c
+++ src/usb_cli.c
@@ -5,6 +5,8 @@
 #include "app_usbd_serial_num.h"
 #include "app_usbd_core.h"
 #include "hsv.h"
+#include "pwm_leds.h"
+#include "nrf_delay.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -21,14 +23,9 @@ static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
 
 #define CDC_ACM_COMM_INTERFACE  0
 #define CDC_ACM_COMM_EPIN       NRF_DRV_USBD_EPIN2
-
 #define CDC_ACM_DATA_INTERFACE  1
 #define CDC_ACM_DATA_EPIN       NRF_DRV_USBD_EPIN1
 #define CDC_ACM_DATA_EPOUT      NRF_DRV_USBD_EPOUT1
-
-static char m_rx_buffer[1];
-static char m_line_buffer[128];
-static uint8_t m_line_idx = 0;
 
 APP_USBD_CDC_ACM_GLOBAL_DEF(m_app_cdc_acm,
                             cdc_acm_user_ev_handler,
@@ -39,23 +36,68 @@ APP_USBD_CDC_ACM_GLOBAL_DEF(m_app_cdc_acm,
                             CDC_ACM_DATA_EPOUT,
                             APP_USBD_CDC_COMM_PROTOCOL_AT_V250);
 
+#define RX_BUF_SIZE 256
+static char m_fifo_buffer[RX_BUF_SIZE];
+static volatile uint16_t m_fifo_head = 0;
+static volatile uint16_t m_fifo_tail = 0;
+
+static char m_usb_rx_char;
+
+static char m_line_buffer[128];
+static uint8_t m_line_idx = 0;
+
+static char m_tx_buffer[512];
+
+static void fifo_put(char c) {
+    uint16_t next_head = (m_fifo_head + 1) % RX_BUF_SIZE;
+    if (next_head != m_fifo_tail) {
+        m_fifo_buffer[m_fifo_head] = c;
+        m_fifo_head = next_head;
+    }
+}
+
+static bool fifo_get(char *c) {
+    if (m_fifo_head == m_fifo_tail) {
+        return false;
+    }
+    *c = m_fifo_buffer[m_fifo_tail];
+    m_fifo_tail = (m_fifo_tail + 1) % RX_BUF_SIZE;
+    return true;
+}
+
+static void usb_send_blocking(const char *data, size_t len) {
+    if (len == 0) return;
+    if (len > sizeof(m_tx_buffer)) len = sizeof(m_tx_buffer);
+    
+    memcpy(m_tx_buffer, data, len);
+    
+    ret_code_t ret;
+    do {
+        ret = app_usbd_cdc_acm_write(&m_app_cdc_acm, m_tx_buffer, len);
+        if (ret == NRF_ERROR_BUSY) {
+            nrf_delay_us(500); 
+        }
+    } while (ret == NRF_ERROR_BUSY);
+}
 
 static void usb_print(const char *msg) {
-    size_t len = strlen(msg);
-    app_usbd_cdc_acm_write(&m_app_cdc_acm, msg, len);
+    usb_send_blocking(msg, strlen(msg));
 }
 
 static void process_command(char *cmd) {
-    char response[100];
+    char temp_resp[128]; 
     char *token = strtok(cmd, " \r\n");
 
-    if (token == NULL) return;
+    if (token == NULL) {
+        usb_print("\r\n> ");
+        return;
+    }
 
     if (strcasecmp(token, "help") == 0) {
-        usb_print("\r\nSupported commands:\r\n");
-        usb_print("  RGB <red> <green> <blue>     Set RGB color (0-1000)\r\n");
-        usb_print("  HSV <hue> <sat> <val>        Set HSV color (0-360, 0-100, 0-100)\r\n");
-        usb_print("  help                         Show this help\r\n");
+        usb_print("\r\nSupported commands:\r\n"
+                  "  RGB <r> <g> <b>    Set RGB (0-1000)\r\n"
+                  "  HSV <h> <s> <v>    Set HSV (0-360, 0-100, 0-100)\r\n"
+                  "  help               Show help\r\n");
     } 
     else if (strcasecmp(token, "RGB") == 0) {
         int r = -1, g = -1, b = -1;
@@ -72,10 +114,10 @@ static void process_command(char *cmd) {
             rgb_to_hsv_simple(r, g, b, &new_hsv.h, &new_hsv.s, &new_hsv.v);
             update_hsv_state(new_hsv);
             
-            sprintf(response, "\r\nColor set to R=%d G=%d B=%d\r\n", r, g, b);
-            usb_print(response);
+            sprintf(temp_resp, "\r\nColor set to R=%d G=%d B=%d\r\n", r, g, b);
+            usb_print(temp_resp);
         } else {
-            usb_print("\r\nUnknown command (Invalid RGB arguments)\r\n");
+            usb_print("\r\nError: RGB values must be 0-1000\r\n");
         }
     }
     else if (strcasecmp(token, "HSV") == 0) {
@@ -92,10 +134,10 @@ static void process_command(char *cmd) {
             hsv_color_t new_hsv = { (uint16_t)h, (uint8_t)s, (uint8_t)v };
             update_hsv_state(new_hsv);
 
-            sprintf(response, "\r\nColor set to H=%d S=%d V=%d\r\n", h, s, v);
-            usb_print(response);
+            sprintf(temp_resp, "\r\nColor set to H=%d S=%d V=%d\r\n", h, s, v);
+            usb_print(temp_resp);
         } else {
-            usb_print("\r\nUnknown command (Invalid HSV arguments)\r\n");
+            usb_print("\r\nError: Use H:0-360, S:0-100, V:0-100\r\n");
         }
     }
     else {
@@ -110,34 +152,19 @@ static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
 {
     switch (event) {
         case APP_USBD_CDC_ACM_USER_EVT_PORT_OPEN:
-            app_usbd_cdc_acm_read(&m_app_cdc_acm, m_rx_buffer, 1);
-            usb_print("\r\nHSV CLI Ready. Type 'help'.\r\n> ");
+            app_usbd_cdc_acm_read(&m_app_cdc_acm, &m_usb_rx_char, 1);
+            pwm_set_rgb_values(0, 0, 1000);
             break;
             
         case APP_USBD_CDC_ACM_USER_EVT_PORT_CLOSE:
+            pwm_set_rgb_values(0, 0, 0);
             break;
             
         case APP_USBD_CDC_ACM_USER_EVT_RX_DONE:
         {
-            char c = m_rx_buffer[0];
+            fifo_put(m_usb_rx_char);
             
-            app_usbd_cdc_acm_write(&m_app_cdc_acm, m_rx_buffer, 1);
-
-            if (c == '\r') {
-                m_line_buffer[m_line_idx] = 0;
-                process_command(m_line_buffer);
-                m_line_idx = 0;
-            } 
-            else if (c == 127 || c == 8) {
-                if (m_line_idx > 0) m_line_idx--;
-            }
-            else if (c >= ' ' && c <= '~') {
-                if (m_line_idx < sizeof(m_line_buffer) - 1) {
-                    m_line_buffer[m_line_idx++] = c;
-                }
-            }
-
-            app_usbd_cdc_acm_read(&m_app_cdc_acm, m_rx_buffer, 1);
+            app_usbd_cdc_acm_read(&m_app_cdc_acm, &m_usb_rx_char, 1);
             break;
         }
         default:
@@ -175,9 +202,7 @@ void cli_init(void)
 
     app_usbd_serial_num_generate();
     app_usbd_init(&usbd_config);
-    
     app_usbd_class_append(app_usbd_cdc_acm_class_inst_get(&m_app_cdc_acm));
-    
     app_usbd_enable();
     app_usbd_start();
 }
@@ -185,5 +210,36 @@ void cli_init(void)
 void cli_process(void)
 {
     while (app_usbd_event_queue_process()) {
+    }
+
+    char c;
+    while (fifo_get(&c)) {
+        
+        pwm_set_rgb_values(0, 1000, 0); 
+
+        if (c != '\r' && c != '\n') {
+             usb_send_blocking(&c, 1);
+        }
+
+        if (c == '\r' || c == '\n') {
+            m_line_buffer[m_line_idx] = 0;
+            if (m_line_idx > 0) {
+                process_command(m_line_buffer);
+            } else {
+                usb_print("\r\n> ");
+            }
+            m_line_idx = 0;
+        } 
+        else if (c == 127 || c == 8) {
+            if (m_line_idx > 0) {
+                m_line_idx--;
+                usb_print("\b \b");
+            }
+        }
+        else if (c >= ' ' && c <= '~') {
+            if (m_line_idx < sizeof(m_line_buffer) - 1) {
+                m_line_buffer[m_line_idx++] = c;
+            }
+        }
     }
 }
